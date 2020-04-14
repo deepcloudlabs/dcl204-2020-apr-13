cecba234182bd4e1712e03a8e7373ee7b7e22190
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-14 10:45:58 +0300

    use Bank class in sample application

--- core-banking/src/com/example/banking/BankApp.java
+++ core-banking/src/com/example/banking/BankApp.java
@@ -1,6 +1,8 @@
 package com.example.banking;
 
 import com.example.banking.domain.Account;
+import com.example.banking.domain.Bank;
+import com.example.banking.domain.Customer;
 
 /**
  * @author Binnur Kurt <binnur.kurt@gmail.com>
@@ -19,5 +21,28 @@ public class BankApp {
         System.out.println(acc.getBalance());
         acc.withdraw(3_000_000);
         System.out.println(acc.toString());
+
+        Bank bank = new Bank(1, "My Bank");
+        Customer jack = bank.createCustomer("1", "Jack Bauer");
+        jack.addAccount(new Account("TR1", 10_000));
+        jack.addAccount(new Account("TR2", 20_000));
+        jack.addAccount(new Account("TR3", 30_000));
+        Customer kate = bank.createCustomer("2", "Kate Austen");
+        // Alt + Shift + Insert : Column selection mode
+        // Esc
+        kate.addAccount(new Account("TR4", 40_000));
+        kate.addAccount(new Account("TR5", 50_000));
+        kate.addAccount(new Account("TR6", 60_000));
+        System.out.println(bank.getTotalBalance8());
+
+        bank.findCustomerByIdentity("2")   // SAFE : NPE free
+            .ifPresent(   // Null Check
+               customer -> customer.findAccountByIban("TR5")
+                                   .ifPresent( account -> account.withdraw(10_000))
+            );
+        Customer cust = bank.findCustomerByIdentity("3")
+                  .orElseThrow( () -> new IllegalArgumentException("No such customer!"));
+        System.out.println(cust.getFullName()); // NPE free : SAFE
+        System.out.println(bank.getTotalBalance8());
     }
 }
