0fdd766ac6ff6d0d58a0233fe66a2163b848d392
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-14 15:47:11 +0300

    initial commit

---
+++ study-interfaces/src/com/example/animals/Animal.java
@@ -0,0 +1,20 @@
+package com.example.animals;
+
+public abstract class Animal {
+    protected int legs;
+
+    public Animal(int legs) {
+        this.legs = legs;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void walk(){ // concrete
+        System.err.println(
+           String.format("Animal with %d legs is walking now...",legs)
+        );
+    }
+    public abstract void eat(); // abstract
+}
