ab4f2ee8f40c6755b44b665735b5b90578e26d42
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-15 18:59:46 +0300

    format source code

--- collections-benchmarking/src/main/java/com/example/benchmarks/ListBenchmark.java
+++ collections-benchmarking/src/main/java/com/example/benchmarks/ListBenchmark.java
@@ -15,117 +15,118 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 // 1. FastList (2315)    27 111
 // 2. ArrayDeque (2288)  84
 // 3. ArrayList (2204)
+
 /**
  * @author Binnur Kurt <binnur.kurt@gmail.com>
  */
 public class ListBenchmark {
 
-	private static final int DATA_SIZE = 100_000;
+    private static final int DATA_SIZE = 100_000;
 
-	@Benchmark
-	public void addTail_FastList() {
-		List<Integer> list = new FastList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(i);
-	}
-	
-	@Benchmark
-	public void addTail_FastList_FullCapacity() {
-		List<Integer> list = new FastList<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(i);
-	}
-	
-	@Benchmark
-	public void addHead_FastList() {
-		List<Integer> list = new FastList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(0,i);
-	}
-	
-	@Benchmark
-	public void addHead_FastList_FullCapacity() {
-		List<Integer> list = new FastList<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(0,i);
-	}
-	
-	@Benchmark
-	public void addTail_ArrayList() {
-		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(i);
-	}
+    @Benchmark
+    public void addTail_FastList() {
+        List<Integer> list = new FastList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(i);
+    }
 
-	@Benchmark
-	public void addTail_ArrayList_fullCapacity() {
-		List<Integer> list = new ArrayList<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(i);
-	}
+    @Benchmark
+    public void addTail_FastList_FullCapacity() {
+        List<Integer> list = new FastList<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(i);
+    }
 
-	@Benchmark
-	public void addTail_ArrayDeque() {
-		Deque<Integer> list = new ArrayDeque<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.addLast(i);
-	}
+    @Benchmark
+    public void addHead_FastList() {
+        List<Integer> list = new FastList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(0, i);
+    }
 
-	@Benchmark
-	public void addTail_ArrayDeque_fullCapacity() {
-		Deque<Integer> list = new ArrayDeque<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.addLast(i);
-	}
+    @Benchmark
+    public void addHead_FastList_FullCapacity() {
+        List<Integer> list = new FastList<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(0, i);
+    }
 
-	@Benchmark()
-	public void addHead_ArrayList() {
-		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(0, i);
-	}
+    @Benchmark
+    public void addTail_ArrayList() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(i);
+    }
 
-	@Benchmark()
-	public void addHead_ArrayList_fullCapacity() {
-		List<Integer> list = new ArrayList<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(0, i);
-	}
+    @Benchmark
+    public void addTail_ArrayList_fullCapacity() {
+        List<Integer> list = new ArrayList<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(i);
+    }
 
-	@Benchmark
-	public void addHead_ArrayDeque() {
-		Deque<Integer> list = new ArrayDeque<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.addFirst(i);
-	}
+    @Benchmark
+    public void addTail_ArrayDeque() {
+        Deque<Integer> list = new ArrayDeque<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.addLast(i);
+    }
 
-	@Benchmark
-	public void addHead_ArrayDeque_fullCapacity() {
-		Deque<Integer> list = new ArrayDeque<>(DATA_SIZE);
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.addFirst(i);
-	}
+    @Benchmark
+    public void addTail_ArrayDeque_fullCapacity() {
+        Deque<Integer> list = new ArrayDeque<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.addLast(i);
+    }
 
-	@Benchmark
-	public void addTail_LinkedList() {
-		List<Integer> list = new LinkedList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(i);
-	}
+    @Benchmark()
+    public void addHead_ArrayList() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(0, i);
+    }
 
-	@Benchmark
-	public void addHead_LinkedList() {
-		List<Integer> list = new LinkedList<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			list.add(0, i);
-	}
+    @Benchmark()
+    public void addHead_ArrayList_fullCapacity() {
+        List<Integer> list = new ArrayList<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(0, i);
+    }
 
-	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder()
-				.include(ListBenchmark.class.getSimpleName())
-				.warmupIterations(5)
-				.measurementIterations(5).forks(1).build();
+    @Benchmark
+    public void addHead_ArrayDeque() {
+        Deque<Integer> list = new ArrayDeque<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.addFirst(i);
+    }
 
-		new Runner(opt).run();
-	}
+    @Benchmark
+    public void addHead_ArrayDeque_fullCapacity() {
+        Deque<Integer> list = new ArrayDeque<>(DATA_SIZE);
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.addFirst(i);
+    }
+
+    @Benchmark
+    public void addTail_LinkedList() {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(i);
+    }
+
+    @Benchmark
+    public void addHead_LinkedList() {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            list.add(0, i);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(ListBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(5).forks(1).build();
+
+        new Runner(opt).run();
+    }
 }
--- collections-benchmarking/src/main/java/com/example/benchmarks/SetBenchmark.java
+++ collections-benchmarking/src/main/java/com/example/benchmarks/SetBenchmark.java
@@ -16,37 +16,37 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  */
 public class SetBenchmark {
 
-	private static final int DATA_SIZE= 250_000;
+    private static final int DATA_SIZE = 250_000;
 
-	@Benchmark
-	public void add_HashSet() {
-		Set<Integer> set = new HashSet<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			set.add(i);
-	}
-	
-	@Benchmark
-	public void add_LinkedHashSet() {
-		Set<Integer> set = new LinkedHashSet<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			set.add(i);
-	}
-	
-	@Benchmark
-	public void add_TreeHashSet() {
-		Set<Integer> set = new TreeSet<>();
-		for (int i = 0; i < DATA_SIZE; ++i)
-			set.add(i);
-	}	
+    @Benchmark
+    public void add_HashSet() {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            set.add(i);
+    }
 
-	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder()
-				.include(SetBenchmark.class.getSimpleName())
-				.warmupIterations(5)
-				.measurementIterations(10)
-				.forks(1)
-				.build();
+    @Benchmark
+    public void add_LinkedHashSet() {
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            set.add(i);
+    }
 
-		new Runner(opt).run();
-	}
+    @Benchmark
+    public void add_TreeHashSet() {
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < DATA_SIZE; ++i)
+            set.add(i);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(SetBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(10)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
 }
