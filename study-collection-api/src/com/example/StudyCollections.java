22b7edb4a7d49255b76b2d0bdf9921df5511e028
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-15 18:58:44 +0300

    initial commit

---
+++ study-collection-api/src/com/example/StudyCollections.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class StudyCollections {
+
+    public static void main(String[] args) {
+	   // Collection API -> Java 1.2
+        // 1. List: Allows duplicates, Ordered -> Sortable
+        // 2. Set:  Does not allow duplicates, Unordered -> Unsorted!
+        // 1 and 2 are Linear Collection
+        // 3. Map : Associative Collection: Key -> Value
+       // Java SE 5 (Generics) List -> List<Integer> x List<int>
+       // Java SE 8 (Stream API, FilterMapReduce, Functional Programming)
+       // Alternative APIs outside JDK
+       // 1. Guava (Google)
+       // 2. Eclipse Collections
+    }
+}
