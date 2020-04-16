1ccc7ecacd18d3db4d15a2b4af5f5c8cf647a2e0
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-16 18:45:29 +0300

    format source code

--- core-banking/test/com/example/banking/domain/CheckingAccountTest.java
+++ core-banking/test/com/example/banking/domain/CheckingAccountTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ */
 class CheckingAccountTest {
 
     @Test
     void withdrawWithNegativeAmountShouldThrowIllegalArgumentException() {
-        CheckingAccount account = new CheckingAccount("TR1",1_000, 500);
+        CheckingAccount account = new CheckingAccount("TR1", 1_000, 500);
         assertThrows(IllegalArgumentException.class, () -> account.withdraw(-1.));
         assertEquals(1_000, account.getBalance());
         assertEquals(500, account.getOverdraftAmount());
@@ -16,7 +19,7 @@ class CheckingAccountTest {
 
     @Test
     void withdrawOverBalanceAndOverdraftShouldThrowInsufficientBalanceException() {
-        CheckingAccount account = new CheckingAccount("TR1",1_000, 500);
+        CheckingAccount account = new CheckingAccount("TR1", 1_000, 500);
         assertThrows(InsufficientBalanceException.class, () -> account.withdraw(1_501.));
         assertEquals(1_000, account.getBalance());
         assertEquals(500, account.getOverdraftAmount());
@@ -24,7 +27,7 @@ class CheckingAccountTest {
 
     @Test
     void withdrawAllBalanceShouldBeOk() throws Throwable {
-        CheckingAccount account = new CheckingAccount("TR1",1_000, 500);
+        CheckingAccount account = new CheckingAccount("TR1", 1_000, 500);
         account.withdraw(1_000.);
         assertEquals(0, account.getBalance());
         assertEquals(500, account.getOverdraftAmount());
@@ -32,7 +35,7 @@ class CheckingAccountTest {
 
     @Test
     void withdrawAllBalanceAndOverdraftAmountShouldBeOk() throws Throwable {
-        CheckingAccount account = new CheckingAccount("TR1",1_000, 500);
+        CheckingAccount account = new CheckingAccount("TR1", 1_000, 500);
         account.withdraw(1_500.);
         assertEquals(-500, account.getBalance());
         assertEquals(500, account.getOverdraftAmount());
