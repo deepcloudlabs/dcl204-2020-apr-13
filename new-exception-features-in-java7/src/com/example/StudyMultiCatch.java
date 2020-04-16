c98c484e1ed3acda6478fd768a494773cfa74670
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-16 14:13:48 +0300

    initial commit

---
+++ new-exception-features-in-java7/src/com/example/StudyMultiCatch.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class StudyMultiCatch {
+    public static void fun() throws E1 {}
+    public static void gun() throws E2 {}
+    public static void sun() throws E3,E4 {}
+    public static void main(String[] args) {
+        try {
+            fun();
+            gun();
+            sun();
+        } catch (E1 | E2 | E3 | E4 e) { // multi-catch
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
+class E1 extends Exception {}
+class E2 extends Exception {}
+class E3 extends Exception {}
+class E4 extends Exception {}
+
--- part2/src/com/example/animals/exercises/Exercise1.java
+++ part2/src/com/example/animals/exercises/Exercise1.java
@@ -3,6 +3,7 @@ package com.example.animals.exercises;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -33,5 +34,21 @@ public class Exercise1 {
 						animal -> Pet.class.isInstance(animal) ? "Pet" : "Wild"
 						,Collectors.counting()));
 		System.out.println(groupByWildOrPetCounts);
+		Map<Integer,Long> groupAnimalsByLegs = animals.stream()
+				   .collect(Collectors.groupingBy(Animal::getLegs,Collectors.counting()));
+		System.out.println(groupAnimalsByLegs);
+		Function<Animal, ? extends Class<? extends Animal>> toClass = Animal::getClass;
+		Map<String,Long> groupAnimalsByClass = animals.stream()
+				   .collect(Collectors.groupingBy(toClass.andThen(Class::getSimpleName),
+						   Collectors.counting()));
+		System.out.println(groupAnimalsByClass);
+		var numOfSpecies = animals.stream().map(Object::getClass)
+				        .map(Class::getSimpleName)
+				        .distinct()
+				        .parallel()
+				        .count();
+		// intermediary methods: Lazy -> filter(), sorted(), distinct(), parallel(), map(), mapToXYZ(), ...
+		// terminal methods : count(), collect(), forEach()
+		System.out.println(numOfSpecies);
 	}
 }
