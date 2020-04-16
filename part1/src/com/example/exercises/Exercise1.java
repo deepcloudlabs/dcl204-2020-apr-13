a273f1b23544eaf522596b79e4b84fc12a297b88
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-16 18:46:28 +0300

    format source

--- part1/src/com/example/exercises/Exercise1.java
+++ part1/src/com/example/exercises/Exercise1.java
@@ -14,43 +14,41 @@ import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise1 {
-	private static final WorldDao worldDAO = InMemoryWorldDao.getInstance();
+    private static final WorldDao worldDAO = InMemoryWorldDao.getInstance();
 
-	public static void main(String[] args) {
-		// Find the continents list
+    public static void main(String[] args) {
+        // Find the continents list
         final Collection<Country> countries = worldDAO.findAllCountries();
-		Comparator<String> dictionaryOrderDesc =
-				(r, s) -> s.compareTo(r); // partial order
-		List<String> continents = countries.stream()
-				 //.map( country -> country.getContinent() ) // Lambda Expression
-				 .map( Country::getContinent ) // Method Reference
-				 .peek(System.out::println)
-				 .distinct()
-				 .peek( continent -> {
-					 System.out.println("After distinct: "+continent);
-				 })
-				 .sorted(dictionaryOrderDesc)
-				 .peek(System.out::println)
-				 .collect(Collectors.toList());
-		System.out.println(continents);
-		Map<String,Long> groupByContinentCounts = countries.stream()
-				 .collect(Collectors.groupingBy(
-				 		     Country::getContinent,
-						     Collectors.counting()
-						 )
-				 );
-		System.out.println(groupByContinentCounts);
-		Map<String,List<Country>> groupByContinentCountries = countries.stream()
-				 .collect(Collectors.groupingBy(
-				 		     Country::getContinent
-						 )
-				 );
-		System.out.println(groupByContinentCountries);
-	}
+        Comparator<String> dictionaryOrderDesc =
+                (r, s) -> s.compareTo(r); // partial order
+        List<String> continents = countries.stream()
+                //.map( country -> country.getContinent() ) // Lambda Expression
+                .map(Country::getContinent) // Method Reference
+                .peek(System.out::println)
+                .distinct()
+                .peek(continent -> {
+                    System.out.println("After distinct: " + continent);
+                })
+                .sorted(dictionaryOrderDesc)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        System.out.println(continents);
+        Map<String, Long> groupByContinentCounts = countries.stream()
+                .collect(Collectors.groupingBy(
+                        Country::getContinent,
+                        Collectors.counting()
+                        )
+                );
+        System.out.println(groupByContinentCounts);
+        Map<String, List<Country>> groupByContinentCountries = countries.stream()
+                .collect(Collectors.groupingBy(
+                        Country::getContinent
+                        )
+                );
+        System.out.println(groupByContinentCountries);
+    }
 
 }
--- part1/src/com/example/exercises/Exercise6.java
+++ part1/src/com/example/exercises/Exercise6.java
@@ -10,31 +10,29 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
- * 
  * @author Binnur Kurt <binnur.kurt@gmail.com>
- *
  */
 public class Exercise6 {
-	private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
+    private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
 
-	public static void main(String[] args) {
-		// Sort the countries by number of their cities in descending order
-		List<Country> countries  = countryDao.findAllCountries();
+    public static void main(String[] args) {
+        // Sort the countries by number of their cities in descending order
+        List<Country> countries = countryDao.findAllCountries();
 
-		Predicate<Country> countriesHavingAtLeastOneCity = country -> country.getCities().size() > 0;
-		Consumer<Country> printCountry = System.out::println;
-		countries.stream()
-				 .filter(countriesHavingAtLeastOneCity)
-				 .sorted( (c1,c2) -> c2.getCities().size() - c1.getCities().size())
-				 .forEach(printCountry);
+        Predicate<Country> countriesHavingAtLeastOneCity = country -> country.getCities().size() > 0;
+        Consumer<Country> printCountry = System.out::println;
+        countries.stream()
+                .filter(countriesHavingAtLeastOneCity)
+                .sorted((c1, c2) -> c2.getCities().size() - c1.getCities().size())
+                .forEach(printCountry);
 
-		Comparator<Country> comparingByCitySize =
-				Comparator.comparing(country -> country.getCities().size());
-		Comparator<Country> comparingByCitySizeDesc = comparingByCitySize.reversed();
-		countries.stream() // DSL : Domain Specific Language
-				 .filter(countriesHavingAtLeastOneCity)
-				 .sorted(comparingByCitySizeDesc)
-				 .forEach(printCountry);
-	}
+        Comparator<Country> comparingByCitySize =
+                Comparator.comparing(country -> country.getCities().size());
+        Comparator<Country> comparingByCitySizeDesc = comparingByCitySize.reversed();
+        countries.stream() // DSL : Domain Specific Language
+                .filter(countriesHavingAtLeastOneCity)
+                .sorted(comparingByCitySizeDesc)
+                .forEach(printCountry);
+    }
 
 }
